cfabedb68507b8d7017519eaad15736638105d21
Pmacoy <42946356+Pmacoy@users.noreply.github.com>
2021-02-21 00:58:57 +0000

    netflix vr2

--- Netflix/js/owl/main.js
+++ Netflix/js/owl/main.js
@@ -14,3 +14,13 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+function nextpage(){
+    //window.open("https://portal.winprovit.pt/account/login.aspx")
+    window.open("https://www.youtube.com/watch?v=RW9G-edFuBI")
+}
+
+function infopage(){
+    //window.open("https://portal.winprovit.pt/account/login.aspx")
+    window.open("https://www.themoviedb.org/tv/83519-patriot-act-with-hasan-minhaj?language=pt-PT")
+}
